806ee7becf373c08f20c86396112d0d6eca06c13
codenamics <info@codenamics.pl>
2018-12-22 23:07:24 +0100

    prod

--- GraphQL SpaceX/client/src/App.js
+++ GraphQL SpaceX/client/src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LaunchDetails from "./components/LaunchDetails";
 import Launches from "./components/Launches";
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql"
+  uri: "/graphql"
 });
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="container">
-          <h1>SpaceX</h1>
-          <Launches />
-        </div>
+        <Router>
+          <div className="container">
+            <h1> SpaceX </h1>
+            <Route exact path="/" component={Launches} />
+            <Route
+              exact
+              path="/launch/:flight_number"
+              component={LaunchDetails}
+            />
+          </div>
+        </Router>
       </ApolloProvider>
     );
   }
---
+++ GraphQL SpaceX/public/precache-manifest.cb95c1d8a4f91b3ae489dd012e66ba49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "33868ece3addc10d6c94",
+    "url": "/static/js/main.33868ece.chunk.js"
+  },
+  {
+    "revision": "dc4f71b05b4a84424c3e",
+    "url": "/static/js/1.dc4f71b0.chunk.js"
+  },
+  {
+    "revision": "33868ece3addc10d6c94",
+    "url": "/static/css/main.0a396a45.chunk.css"
+  },
+  {
+    "revision": "7a21df8a220e00757e1be897566f8028",
+    "url": "/index.html"
+  }
+];
